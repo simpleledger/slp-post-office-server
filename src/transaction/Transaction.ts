2302e0ac3f4fb530bbcac6a9045a44045eb1f042
Andre Cabrera <andrecabrera@protonmail.ch>
2020-09-23 02:35:23 -0300

    refactor(General): Fix Transaction and Network classes, cleanup cli, install packages and test suite.]

--- src/transaction/Transaction.ts
+++ src/transaction/Transaction.ts
@@ -1,21 +1,18 @@
-import errorMessages from './errorMessages'
-import config from '../config.json'
+import errorMessages from '../errorMessages'
+import config from '../../config.json'
 import BCHJS from '@chris.troutner/bch-js'
 import BigNumber from 'bignumber.js'
 import ITransaction from './ITransaction'
 const { TransactionBuilder, ECSignature } = require('bitcoincashjs-lib')
 
-class Transaction implements ITransaction {
+export default class Transaction implements ITransaction {
     static MIN_BYTES_INPUT = 181
     static LOKAD_ID_INDEX = 1
     static TOKEN_ID_INDEX = 4
     static LOKAD_ID_INDEX_VALUE = '534c5000'
     static SLP_OP_RETURN_VOUT = 0
 
-    static bchjs = new BCHJS({
-        restURL: config.network === 'mainnet' ? 'https://api.fullstack.cash/v3/' : 'https://tapi.fullstack.cash/v3/',
-        apiToken: config.apiKey,
-    })
+    static bchjs = new BCHJS()
 
     
     addStampsForTransactionAndSignInputs(transaction: any, keyPairFromPostOffice: any, stamps: any): any {
@@ -42,15 +39,15 @@ class Transaction implements ITransaction {
 
     getNeededStamps(transaction: any): number {
         BigNumber.set({ ROUNDING_MODE: BigNumber.ROUND_UP })
-        const transactionScript = bchjs.Script.toASM(transaction.outs[Transaction.SLP_OP_RETURN_VOUT].script).split(' ')
+        const transactionScript = Transaction.bchjs.Script.toASM(transaction.outs[Transaction.SLP_OP_RETURN_VOUT].script).split(' ')
         if (transactionScript[Transaction.LOKAD_ID_INDEX] !== Transaction.LOKAD_ID_INDEX_VALUE)
             throw new Error(errorMessages.INVALID_SLP_OP_RETURN)
     
         let neededStamps = 0
         let tokenOutputPostage = 0
         for (let i = 1; i < transaction.outs.length; i++) {
-            const addressFromOut = bchjs.SLP.Address.toSLPAddress(
-                bchjs.Address.fromOutputScript(transaction.outs[i].script),
+            const addressFromOut = Transaction.bchjs.SLP.Address.toSLPAddress(
+                Transaction.bchjs.Address.fromOutputScript(transaction.outs[i].script),
             )
             const postOfficeAddress = config.postageRate.address
             if (postOfficeAddress === addressFromOut) tokenOutputPostage = Transaction.TOKEN_ID_INDEX + i
@@ -80,31 +77,31 @@ class Transaction implements ITransaction {
     }
 
     splitUtxosIntoStamps(utxos: any, hdNode: any) {
-            const transactionBuilder = config.network === 'mainnet' ? new bchjs.TransactionBuilder() : new bchjs.TransactionBuilder('testnet')
+            const transactionBuilder = config.network === 'mainnet' ? new Transaction.bchjs.TransactionBuilder() : new Transaction.bchjs.TransactionBuilder('testnet')
 
         const originalAmount = utxos.reduce((accumulator, utxo) => accumulator + utxo.value, 0)
 
         const numberOfPossibleStamps = originalAmount / (config.postageRate.weight + Transaction.MIN_BYTES_INPUT)
-        const hypotheticalByteCount = bchjs.BitcoinCash.getByteCount(
+        const hypotheticalByteCount = Transaction.bchjs.BitcoinCash.getByteCount(
             { P2PKH: utxos.length },
             { P2PKH: numberOfPossibleStamps },
         )
         const satoshisPerByte = 1.4
         const hypotheticalTxFee = Math.floor(satoshisPerByte * hypotheticalByteCount)
-        let numberOfActualStamps = (originalAmount - hypotheticalTxFee) / (config.postageRate.weight + MIN_BYTES_INPUT)
+        let numberOfActualStamps = (originalAmount - hypotheticalTxFee) / (config.postageRate.weight + Transaction.MIN_BYTES_INPUT)
         if (numberOfActualStamps > 100) {
             numberOfActualStamps = 50
         }
 
         utxos.forEach(utxo => transactionBuilder.addInput(utxo.tx_hash, utxo.tx_pos))
-        const keyPair = bchjs.HDNode.toKeyPair(hdNode)
-        const outputAddress = bchjs.HDNode.toCashAddress(hdNode)
-        const byteCount = bchjs.BitcoinCash.getByteCount({ P2PKH: utxos.length }, { P2PKH: numberOfActualStamps })
+        const keyPair = Transaction.bchjs.HDNode.toKeyPair(hdNode)
+        const outputAddress = Transaction.bchjs.HDNode.toCashAddress(hdNode)
+        const byteCount = Transaction.bchjs.BitcoinCash.getByteCount({ P2PKH: utxos.length }, { P2PKH: numberOfActualStamps })
         const txFee = Math.floor(satoshisPerByte * byteCount)
         const totalSatoshisToSend = (config.postageRate.weight + Transaction.MIN_BYTES_INPUT) * numberOfActualStamps
 
         for (let i = 0; i < numberOfActualStamps; i++) {
-            transactionBuilder.addOutput(outputAddress, config.postageRate.weight + MIN_BYTES_INPUT)
+            transactionBuilder.addOutput(outputAddress, config.postageRate.weight + Transaction.MIN_BYTES_INPUT)
         }
         const change = originalAmount - totalSatoshisToSend - txFee
         if (change > 1082) {

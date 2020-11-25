f3f76e88e78cbc7993e70956578512e2fc626f67
blockparty <hello@blockparty.sh>
2020-11-25 15:38:50 +0000

    remove unused interface

--- src/network/INetwork.d.ts
+++ src/network/INetwork.d.ts
@@ -1,10 +1,6 @@
-interface INetowrkConstructor {
-    new (config: any): INetwork;
-}
-
 export default interface INetwork {
-    fetchUTXOsForStampGeneration: (cashAddress: string) => any;
-    fetchUTXOsForNumberOfStampsNeeded: (numberOfStamps: number, cashAddress: string) => any;
-    validateSLPInputs: (inputs: any) => any;
-    broadcastTransaction: (rawTransactionHex: string) => any;
+    fetchUTXOsForStampGeneration: (cashAddress: string) => any
+    fetchUTXOsForNumberOfStampsNeeded: (numberOfStamps: number, cashAddress: string) => any
+    validateSLPInputs: (inputs: any) => any
+    broadcastTransaction: (rawTransactionHex: string) => any
 }

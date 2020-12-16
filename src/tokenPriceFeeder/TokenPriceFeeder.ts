78e6e1b39ac71c996ac9f200634097b9b8a1eefa
OPReturnCode <mask.em@protonmail.com>
2020-12-16 00:05:42 +0000

    add an option to use the initial stamp rate as min rate to TokenPriceFeeder, #23

--- src/tokenPriceFeeder/TokenPriceFeeder.ts
+++ src/tokenPriceFeeder/TokenPriceFeeder.ts
@@ -1,3 +1,4 @@
+import BigNumber from 'bignumber.js'
 import IApiWrapper from './ApiWrapper/IApiWrapper'
 
 export default class TokenPriceFeeder {
@@ -5,6 +6,8 @@ export default class TokenPriceFeeder {
     private tickInSeconds: number
     private tokenId: string
     private apiWrapper: IApiWrapper
+    private useInitialStampRateAsMin: boolean
+    private initialStampRate: BigNumber
     private _applyCustomRule: (price: number) => number
 
     private _applyDefaultRule(price: number): number {
@@ -12,12 +15,27 @@ export default class TokenPriceFeeder {
         return price * minerFeeInUSD
     }
 
-    public constructor(config: any, tickInSeconds: number,tokenId: string, apiWrapper: IApiWrapper, applyCustomRule: (price: number) => number) {
-        this.config = config
-        this.tickInSeconds = tickInSeconds
-        this.tokenId = tokenId
-        this.apiWrapper = apiWrapper
-        this._applyCustomRule = applyCustomRule
+    public constructor(
+        config: any,
+        tickInSeconds: number,
+        tokenId: string,
+        apiWrapper: IApiWrapper,
+        useInitialStampRateAsMin: boolean = false,
+        applyCustomRule?: (price: number) => number
+        ) {
+            this.config = config
+            this.tickInSeconds = tickInSeconds
+            this.tokenId = tokenId
+            this.apiWrapper = apiWrapper
+            this._applyCustomRule = applyCustomRule
+            this.useInitialStampRateAsMin = useInitialStampRateAsMin
+
+            this.config.postageRate.stamps.forEach(stamp => {
+                if (stamp.tokenId === this.tokenId) {
+                    this.initialStampRate = new BigNumber(stamp.rate)
+                }
+            });
+
     }
 
     public async run() {
@@ -32,8 +50,15 @@ export default class TokenPriceFeeder {
                     } else {
                         price = this._applyDefaultRule(priceData)
                     }
-                    stamp.rate = new BigNumber(price).times(10 ** stamp.decimals)
-                    
+                    let newStampRate: BigNumber = new BigNumber(price).times(10 ** stamp.decimals)
+
+                    if (this.useInitialStampRateAsMin) {
+                        if (newStampRate.lt(this.initialStampRate)) {
+                            newStampRate = this.initialStampRate
+                        }
+                    }
+
+                    stamp.rate = newStampRate  
                 }
             });
             
